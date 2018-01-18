71db36b460f881849bbb8797d2b079f1f81f8542
wl19930525 <958337798@qq.com>
2018-01-18 16:51:51 +0800

    新的数据

---
+++ app/src/main/java/com/cn/yingxionglianm/MainActivity.java
@@ -0,0 +1,18 @@
+package com.cn.yingxionglianm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String str = "112233";
+        String str1 = "112233";
+        String str2 = "112233";
+        String str3 = "112233";
+    }
+}
